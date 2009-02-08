f381ad61397e2000023e09e43b9498ccefe1f332
Alex MacCaw <maccman@gmail.com>
2009-02-08 11:15:24 +0000

    Add PHP client

---
+++ moderate.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ *
+ * PHP client to SocialMod
+ * For more information, please see the API:
+ * http://socialmod.com/api
+ *
+ * Dependencies:
+ * - curl
+ * - my version of ActiveResource.php
+ * 
+ * Usage:
+ *
+ * <?php
+ *
+ * require_once ('moderate.php');
+ * $mod = new Moderate(array('src' => 'http://example.com'));
+ * $mod->save();
+ *
+ * @author Alex MacCaw (info@socialmod.com)
+ * @version 0.1
+ * @license MIT
+ *
+ */
+
+require_once ('ActiveResource.php');
+
+class Moderate extends ActiveResource {
+    var $site = 'http://api.socialmod.com/';
+    var $element_name = 'items';
+    
+    function set_api_key($key){
+      $this->headers = array("Authorization:".$key);
+    }
+    
+    function flag(){
+      $this->post('flag');
+    }
+}
+
+?>
